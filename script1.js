46160b3d879ad538c00cac597fd76566a751cb72
Chris Johnson <111929631+chrisjohnsonqe@users.noreply.github.com>
2024-03-27 16:16:34 +1100

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+//this is the welcome message
+document.write('Welcome to the Hawker site!');
+
+// this is Hello pop up
+alert('Hello from Hawker');
+
+//console logmessage
+console.log("it worked")
